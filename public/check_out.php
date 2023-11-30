e274620bb908f93b3cb791393c1658f2ce33d512
quangk22 <quangcute504@gmail.com>
2023-11-30 09:44:12 +0700

    add

--- public/check_out.php
+++ public/check_out.php
@@ -1,16 +1,20 @@
 <?php 
 include_once '../core/db/db_products.php';
-include_once '../core/db/db_order_items.php';
+
 session_start();
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    
 }
 if($_SERVER['REQUEST_METHOD'] === 'GET'){
     $productList = get_all_products();
+    $_SESSION['isCheck'] = false;
 
     if (isset($_SESSION['email']) && $_SESSION['email'] != "") {
         include_once './view/_check_out.php';
         
     } else {
+        $_SESSION['isCheck'] = true;
         include_once './view/_account.php';
     }
 }
